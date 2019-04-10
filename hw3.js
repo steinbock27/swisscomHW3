d4cd8beeac9d42d5edbe78bd2136375e00745ecd
steinbock27 <ismetferdi@gmail.com>
2019-04-10 13:04:42 +0200

    javascript code added...

---
+++ hw3.js
@@ -0,0 +1,22 @@
+
+function changeTitle(){
+var input= document.getElementById('inputField').value
+  if(input){
+
+    document.getElementById('title').innerText= input;
+}
+
+}
+
+function clickButton1(){
+
+      document.getElementById('heading2').innerText= "You clicked Button 1!";
+  }
+function clickButton2() {
+
+      document.getElementById('heading2').innerText= "You clicked Button 2!";
+  }
+function clickButton3() {
+      document.getElementById('heading2').innerText= "You clicked Button 3!";
+
+  }
